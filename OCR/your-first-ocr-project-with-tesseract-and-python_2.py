46a62049160662b5d04f2ae44bff06add4cf1844
drakeredwind01 <drakeredwind01@gmail.com>
2023-11-11 15:20:01 -0800

    2023.11.11.14.27.10.117     herb details

--- OCR/your-first-ocr-project-with-tesseract-and-python_2.py
+++ OCR/your-first-ocr-project-with-tesseract-and-python_2.py
@@ -3,7 +3,8 @@ import cv2
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 # image stuff
-img = cv2.imread("20231010_130302.jpg")
+img = cv2.imread("20231010_130254.jpg")
+
 # img = cv2.resize(img, (400, 400))
 cv2.imshow("Image", img)
 # using pytesseract to get text
