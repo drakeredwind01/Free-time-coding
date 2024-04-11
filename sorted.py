35052181646790877606d109af4ba8cf08c7bc4f
drakeredwind01 <drakeredwind01@gmail.com>
2024-04-10 22:46:10 -0700

    sorted.py formated

--- sorted.py
+++ sorted.py
@@ -1,3 +1,5 @@
-secops = [    "Analytics and data",     "Balancers",     "DNS",     "Load",     "Network agility",     "Orchestration and automation",     "Security",     "Single Source of Truth (SOT)",     "configuration",     "monitoring",     "provisioning",     "troubleshooting",]
+secops = ["Analytics and data", "Balancers", "DNS", "Load", "Network agility", "Orchestration and automation",
+          "Security", "Single Source of Truth (SOT)", "configuration", "monitoring", "provisioning",
+          "troubleshooting", ]
 for item in sorted(set([item.title() for item in secops])):
-     print(item)
+    print(item)
