91d10694513a54597a9f074616d289765c9d896c
drakeredwind01 <drakeredwind01@gmail.com>
2024-04-10 22:45:39 -0700

    sorted.py

---
+++ sorted.py
@@ -0,0 +1,3 @@
+secops = [    "Analytics and data",     "Balancers",     "DNS",     "Load",     "Network agility",     "Orchestration and automation",     "Security",     "Single Source of Truth (SOT)",     "configuration",     "monitoring",     "provisioning",     "troubleshooting",]
+for item in sorted(set([item.title() for item in secops])):
+     print(item)
