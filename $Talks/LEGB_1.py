70137e63baedda915e1055e64f48dcc1637ba1e5
drakeredwind01 <drakeredwind01@gmail.com>
2025-03-27 17:40:14 -0700

    commiting LEGB for talk

--- $Talks/LEGB_1.py
+++ $Talks/LEGB_1.py
@@ -4,7 +4,8 @@ DOG_AGE = 1
 def dog_grow():
     global DOG_AGE
     DOG_AGE += 1
+    # Use the global variable
     print(f"Dog grew to {DOG_AGE} years old.")
-
+    # Dog grew to 2 years old.
 # Call the function to make it print something
 dog_grow()
--- $Talks/LEGB_2.py
+++ $Talks/LEGB_2.py
@@ -3,11 +3,13 @@
 import LEGB_1
 
 print(f"Dog's starting age: {LEGB_1.DOG_AGE}")
+# Dog grew to 2 years old.
 
 LEGB_1.dog_grow()
 LEGB_1.dog_grow()
 
 LEGB_1.DOG_AGE = 5  # Manually age the dog
 
+DOG_AGE = 1
 print(f"Dog's current age: {LEGB_1.DOG_AGE}")
 LEGB_1.dog_grow() # show the new age is used.
