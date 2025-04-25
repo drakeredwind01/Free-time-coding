333ef6ef05d32ab424fb3d05d935a1bec4929046
drakeredwind01 <drakeredwind01@gmail.com>
2025-04-24 17:56:07 -0700

    LEGB_3.py LEGB_4.py

---
+++ $Talks/LEGB_3.py
@@ -0,0 +1,17 @@
+# LEGB_3.py
+DOG_NAME = "Fido"  # Initialize DOG_NAME without printing yet
+DOG_AGE = 1
+
+def name_dog():
+    global DOG_NAME
+    DOG_NAME = "ACE"
+    print(f"Dog's name changed to {DOG_NAME}")
+
+def dog_grow():
+    global DOG_AGE
+    DOG_AGE += 1
+    print(f"Dog grew to {DOG_AGE} years old.")
+
+def print_dog_data(): #Added Function
+    print(f"all the following data is for {DOG_NAME}")
+    dog_grow()
---
+++ $Talks/LEGB_4.py
@@ -0,0 +1,16 @@
+# LEGB_4.py
+import LEGB_3
+
+LEGB_3.DOG_NAME = "ACE" #rename before LEGB_3 prints
+
+LEGB_3.print_dog_data()
+
+print(f"Dog's starting age: {LEGB_3.DOG_AGE}")
+
+LEGB_3.dog_grow()
+LEGB_3.dog_grow()
+
+LEGB_3.DOG_AGE = 5
+
+print(f"Dog's current age: {LEGB_3.DOG_AGE}")
+LEGB_3.dog_grow()
